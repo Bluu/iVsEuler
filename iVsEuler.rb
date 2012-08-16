d07a18fa008a24e11899cbb0e8292c4e6609f2e1
Bluub1t <bluu@me.com>
2012-08-16 14:25:49 -0700

    Problem 18 Solved

--- iVsEuler.rb
+++ iVsEuler.rb
@@ -74,7 +74,11 @@ puts 'Problem 15 Result = ' + problem15Result.result.to_s()
 problem16Result = Problem16.new
 problem16Result.DoMath
 puts 'Problem 16 Result = ' + problem16Result.result.to_s()
-=end
+
 problem17Result = Problem17.new
 problem17Result.DoMath
 puts 'Problem 17 Result = ' + problem17Result.result.to_s()
+=end
+problem18Result = Problem18.new
+problem18Result.DoMath
+puts 'Problem 18 Result = ' + problem18Result.result.to_s() 
